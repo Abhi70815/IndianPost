e495018ad1a3b1b0ac4b9e887b0232fec38c8324
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-18 00:25:05 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import Home from './home'
+
 import './App.css'
-import PostList from './PostList'
+
 import Routerlist from './Routerlist'
 
 const App = () => {
