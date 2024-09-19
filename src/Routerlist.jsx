085da1f7fe23894be8a23c4188db0652d260130a
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-09-19 19:21:47 +0530

    save

--- src/Routerlist.jsx
+++ src/Routerlist.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import PostList from './PostList'
-import Home from './home'
+import Home from './Home'
 
 const Routerlist = () => {
   return (
     <Routes>
-        <Route path='' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/postlist' element={<PostList/>}/>
     </Routes>
   )
